d06529eb07fd149006b0a38ca485c4c02476c1c2
Kathryn Panger <kpanger88@gmail.com>
2020-08-02 17:02:05 -0700

    function to fetch states

--- D3_data_journalism/static/js/app.js
+++ D3_data_journalism/static/js/app.js
@@ -1,4 +1,9 @@
 // bring in the data
 data = d3.csv("static/data/data.csv").then(function(data){
-    console.log(data[0]);
+states = data.map(function(d) { 
+    return d.state
 })
+console.log(states)
+})
+
+//var newarray=dataArray.forEach(element => console.log(element.x));
