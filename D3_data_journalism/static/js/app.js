46cfa6335cbc2fb736602027f4f699106018ad92
Kathryn Panger <kpanger88@gmail.com>
2020-08-02 17:11:39 -0700

    map all data to arrays

--- D3_data_journalism/static/js/app.js
+++ D3_data_journalism/static/js/app.js
@@ -1,9 +1,81 @@
-// bring in the data
+// bring in the data and store to arrays
 data = d3.csv("static/data/data.csv").then(function(data){
 states = data.map(function(d) { 
     return d.state
+
 })
-console.log(states)
+id = data.map(function(d) { 
+    return d.id
+
 })
+abbr = data.map(function(d) { 
+    return d.abbr
+
+})
+poverty = data.map(function(d) { 
+    return d.poverty
+
+})
+povertyMoe = data.map(function(d) { 
+    return d.povertyMoe
+
+})
+age= data.map(function(d) { 
+    return d.age
+
+})
+ageMoe = data.map(function(d) { 
+    return d.ageMoe
+
+})
+income = data.map(function(d) { 
+    return d.income
+
+})
+incomeMoe = data.map(function(d) { 
+    return d.idcomeMoe
+
+})
+healthcare = data.map(function(d) { 
+    return d.healthcare
+
+})
+healthcareLow = data.map(function(d) { 
+    return d.healthcareLow
+
+})
+healthcareHigh = data.map(function(d) { 
+    return d.healthcareHigh
+
+})
+obesity = data.map(function(d) { 
+    return d.obesity
+
+})
+obesityLow = data.map(function(d) { 
+    return d.obesityLow
+
+})
+obesityHigh = data.map(function(d) { 
+    return d.obesityHigh
+
+})
+smokes = data.map(function(d) { 
+    return d.smokes
+
+})
+smokesLow = data.map(function(d) { 
+    return d.smokesLow
+
+})
+smokesHigh = data.map(function(d) { 
+    return d.smokesHigh
+
+})
+// create a trace for the initial scatterplot
+
+
+console.log(smokesLow)
+});
 
 //var newarray=dataArray.forEach(element => console.log(element.x));
